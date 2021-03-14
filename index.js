5c3176c68a3fe70a9b804dc8752ebc6faab9f1e2
iljuhenson <ilyapavluk24@gmail.com>
2021-03-14 18:26:54 +0300

    hamburger menu?

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+    
     console.log("loaded");
     const burgerMenu = document.querySelector(".nav-burger");
     burgerMenu.addEventListener("click", function(e) {
         e.preventDefault();
         console.log("clicked");
-        burgerMenu.classList.add("nav-burger__active")
+        burgerContent = burgerMenu.children[0];
+        navigation = document.querySelector(".list-wrapper");
+        if(burgerContent.classList.contains("nav-burger__active")) {
+            burgerContent.classList.remove("nav-burger__active");
+            navigation.style.display = "none";
+            setTimeout(navigation.children[0].classList.remove("nav-list__active"), 2000);
+        } else {
+            burgerContent.classList.add("nav-burger__active");
+            navigation.style.display = "block";
+            navigation.children[0].classList.add("nav-list__active");
+        }
     })
 });
